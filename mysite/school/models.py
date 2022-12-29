873c269b73829860af7114dfb649480402c175de
junhyuk1229 <junhyuk1229@gmail.com>
2022-12-29 19:19:38 +0900

    Update Profile change

--- mysite/school/models.py
+++ mysite/school/models.py
@@ -7,7 +7,6 @@ class Teacher(models.Model):
     teacher_name = models.CharField(max_length=100, help_text="Teachers full name. First name first")
     register_date = models.DateTimeField('date_registered', default=timezone.now, help_text="Registered date to school")
     
-
     def __str__(self):
         return f"name: {self.teacher_name}\nregistered date: {self.register_date}"
 
@@ -19,3 +18,5 @@ class Student(models.Model):
 
     def __str__(self):
         return f"Name: {self.student_name}\nRegistered date: {self.register_date}\nAdvisor: {self.advisor_teacher.teacher_name}"
+
+
